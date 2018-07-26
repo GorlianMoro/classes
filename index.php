f36ebdcae124d4d4bb562f72c9b054d1b5b73556
Maxim <legckostupov.maxim@yandex.ru>
2018-07-26 14:41:01 +0300

    second variant classes

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
-
 <?php
 
 echo "Инкапсуляция это свойство системы, позволяющая объединять объекты, методы, классы и т. д. в блоки данных, а так же создавать иерархию доступа к элементам блоков данных для внешнего пользователя.";
 
- echo "У объектов есть следующие плюсы: <br>
- Объекты передаются по ссылке <br>
- Каждый объект имеет свои свойства и методы <br><br>
- Также у объектов есть минусы:<br>
- Они не могут использоваться отдельно от класса т. к. являются её экземпляром <br><br>";
+echo "У объектов есть следующие плюсы: <br>
+Объекты передаются по ссылке <br>
+Каждый объект имеет свои свойства и методы <br><br>
+Также у объектов есть минусы:<br>
+Они не могут использоваться отдельно от класса т. к. являются её экземпляром <br><br>";
 
-class Car {
+class Car
+{
   public $name;
   public $color = 'black';
   public $type = 'sedan';
-  public function getCar() {
-    if ($this->color) {
+  public function getCar()
+  {
+    if ($this->color)
+    {
       return  $color = 'blue';
     }
-    else {
+    else
+    {
       return $color;
     }
   }
@@ -41,24 +44,30 @@ echo $reno->name . '<br>';
 echo $reno->type . '<br>';
 echo $reno->color . '<br><br>';
 
-class Television {
+class Television
+{
   public $color;
   public $name;
   public $diagonal = 28;
-  public function getTelevision() {
+  public function getTelevision()
+  {
     $this->diagonal = 32;
+  }
+  public function __construct(
+    $color,
+    $name
+    )
+  {
+    $this->color = $color;
+    $this->name = $name;
   }
 }
 
-$tv1 = new Television();
-$tv1->name = 'Samsung_bw265739';
-$tv1->color = 'gray';
+$tv1 = new Television('gray', 'Samsung_bw265739');
 $tv1->diagonal;
 
-$tv2 = new Television();
+$tv2 = new Television('black', 'LG_hc6811387');
 $tv2->getTelevision();
-$tv2->name = 'LG_hc6811387';
-$tv2->color = 'black';
 $tv2->diagonal;
 
 echo $tv1->name . '<br>';
@@ -70,39 +79,43 @@ echo $tv2->diagonal . '<br><br>';
 
 class Pen {
   public $color;
-  public function getPen() {
-    if ($this->color == 'black') {
+  public function __construct($color)
+  {
+    $this->color = $color;
+  }
+  public function getPen()
+  {
+    if ($this->color == 'black')
+    {
     return  $color = 'multicolor';
     }
-    else {
+    else
+    {
       return $color = 'green';
     }
   }
 }
 
-$pen1 = new Pen();
-$pen2 = new Pen();
-$pen1->color = 'black';
+$pen1 = new Pen('black');
+$pen2 = new Pen('red');
 $pen1->getPen();
-$pen2->color = 'red';
 $pen2->getPen();
 
 echo $pen1->color . '<br>';
-echo $pen2->color . '<br>';
 echo $pen1->getPen() . '<br>';
+echo $pen2->color . '<br>';
 echo $pen2->getPen() . '<br><br>';
 
 class Duck {
-  public $type = 'Domestic';
-  public function getDuck() {
-    $this->type = 'Wood';
+  public $type;
+  public function __construct($type) {
+    $this->type = $type;
   }
 }
 
-$duck1 = new Duck();
-$duck1->getDuck();
+$duck1 = new Duck('Wood');
 $duck1->type;
-$duck2 = new Duck();
+$duck2 = new Duck('Domestic');
 $duck2->type;
 
 echo $duck1->type . '<br>';
@@ -112,20 +125,27 @@ class Product {
   public $name;
   public $type;
   private $price = 500;
-  public function getProduct() {
-    $this->name = 'Asus_Geforce_GTX1060' ;
+  public function __construct(
+    $name,
+    $type)
+  {
+    $this->name = $name;
+    $this->type = $type;
+  }
+  public function getProduct()
+  {
+    if ($this->name == 'Gigabyte_Geforce_GTX980') {
+      return $name = 'Asus_Geforce_GTX1060';
+    }
   }
 }
-$product1 = new Product();
-$product2 = new Product();
-$product1->name = 'Intel_4770';
-$product2->name = 'Gigabyte_Geforce_GTX980';
+$product1 = new Product('Intel_4770', 'CPU');
+$product2 = new Product('Gigabyte_Geforce_GTX980', 'GPU');
 $product2->getProduct();
-$product1->type = CPU;
-$product2->type = GPU;
+
 
 echo $product1->name . '<br>';
 echo $product1->type . '<br>';
 echo $product2->name . '<br>';
 echo $product2->type . '<br>';
- ?>
+echo $product2->getProduct();
